3368afc736c74ed7b57356e2a503c4c01d9a429f
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2024-09-02 23:42:59 +0100

    add auth methods

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,12 +25,14 @@ def request_filter():
     """
 
     if auth is None:
-        return None
+        pass
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth.require_auth(request.path, excluded_paths):
         pass
-    else:
+    if auth.authorization_header(request) is None:
         abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(401)
