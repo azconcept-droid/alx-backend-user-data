f990b546fe774d385cdaf64d09eb374d5eb44075
Yahaya Azeez <azconcept2016@gmail.com>
2024-09-03 18:12:21 +0100

    fix pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -26,7 +26,9 @@ def request_filter():
 
     if auth is None:
         pass
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
     if auth.require_auth(request.path, excluded_paths):
         pass
     if auth.authorization_header(request) is None:
