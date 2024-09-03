12267a01cb5414c5b7e2afa7e23a21189f842a0d
Yahaya Azeez <azconcept2016@gmail.com>
2024-09-03 20:10:47 +0100

    add more methods

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,3 +22,23 @@ class BasicAuth(Auth):
         base64_token = authorization_header.split(' ')[1]
 
         return base64_token
+    
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """ decode base64 auth token """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            pass
+        except:
+            pass
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract user credentials """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
