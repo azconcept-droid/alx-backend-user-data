b314bf4576f435a798d0f3e5d955a7a4a41eba4a
Yahaya Azeez <azconcept2016@gmail.com>
2024-09-05 22:10:22 +0100

    feat(session auth): add another url to exclude

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,7 +34,7 @@ def request_filter():
         return
 
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
@@ -42,6 +42,9 @@ def request_filter():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    if auth.session_cookie(request) is None:
+        abort(401)
+
     request.current_user = auth.current_user(request)
 
 
