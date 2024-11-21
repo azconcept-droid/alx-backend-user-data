9e4f9a01ecf348b825a4dece4cf9416d53bdfc73
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-21 12:30:51 +0100

    find user by email

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -38,3 +40,17 @@ class DB:
         self._session.commit()
 
         return test_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find user by arbitrary input data arg"""
+        if 'email' in kwargs:
+            query = self._session.query(User)\
+                .filter(User.email.like(kwargs['email']))\
+                .order_by(User.id)
+        else:
+            raise InvalidRequestError
+
+        if query.first() is None:
+            raise NoResultFound
+
+        return query.first()
