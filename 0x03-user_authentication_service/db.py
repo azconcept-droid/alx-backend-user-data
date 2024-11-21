ffaa5df7b5df7f699e829526b5f93f44c6f938ea
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-21 15:23:59 +0100

    change user_id annotation to int and add main file

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -60,7 +60,7 @@ class DB:
 
         return user
 
-    def update_user(self, user_id: str, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """Update user info"""
         for key in kwargs:
             user = self.find_user_by(id=user_id)
