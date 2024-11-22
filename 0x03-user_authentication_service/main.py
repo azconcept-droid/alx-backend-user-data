f5ec1e93d2f2913f7b62dac1d43a092ab4c19974
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-22 02:08:09 +0100

    main.py for e2e test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,19 +1,62 @@
 #!/usr/bin/env python3
 """
-Main file
+Main e2e testing
 """
-from db import DB
-# from user import User
 
-# print(User.__tablename__)
 
-# for column in User.__table__.columns:
-#     print("{}: {}".format(column, column.type))
+def register_user(email: str, password: str) -> None:
+    """ test register user"""
+    pass
 
-my_db = DB()
 
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd",)
-print(user_1.id)
+def log_in_wrong_password(email: str, password: str) -> None:
+    """ Test wrong password """
+    pass
 
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
+
+def log_in(email: str, password: str) -> str:
+    """ login test """
+    pass
+
+
+def profile_unlogged() -> None:
+    """ profile logout test """
+    pass
+
+
+def profile_logged(session_id: str) -> None:
+    """ profile logging test """
+    pass
+
+
+def log_out(session_id: str) -> None:
+    """ log out test"""
+    pass
+
+
+def reset_password_token(email: str) -> str:
+    """ reset password test """
+    pass
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """ update password """
+    pass
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
