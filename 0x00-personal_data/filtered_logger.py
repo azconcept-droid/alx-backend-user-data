fe104683d806a0310e4757872d4a31a70068280a
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-21 11:51:14 +0100

    feature(log) format logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,8 +2,28 @@
 """
 Log filter
 """
-from typing import List
+from typing import List, Tuple
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Tuple[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self._fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format log record """
+        message = filter_datum(self._fields, self.REDACTION,
+                               super().format(record), self.SEPARATOR)
+        return message
 
 
 def filter_datum(fields: List[str], redaction: str,
