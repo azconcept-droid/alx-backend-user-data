0bd53826885d38da54e86421ce5c204fa758a58d
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-21 12:22:02 +0100

    feature(logs): add logger

--- 0x00-personal_data/README.md
+++ 0x00-personal_data/README.md
@@ -1,2 +1,5 @@
 # 0x00. Personal data
 ``Back-end`` ``Authentification``
+
+- [What Is PII, non-PII, and personal data?](https://piwik.pro/blog/what-is-pii-personal-data/)
+- [Uncovering Password Habits](https://www.digitalguardian.com/blog)
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,9 @@ import re
 import logging
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
     """
@@ -33,3 +36,18 @@ def filter_datum(fields: List[str], redaction: str,
         replacement = field + "=" + redaction + separator
         message = re.sub(field + "=.*?" + separator, replacement, message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """ Get logger """
+    logger = logging.getLogger("user_data")
+
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    logger.addHandler(handler)
+
+    return logger
