beed558315503605da87118ea16c0ec5c08d73bc
Yahaya Azeez <azconcept2016@gmail.com>
2025-02-21 20:01:51 +0100

    feature(hashed_password) hash password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ hash password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ validate password """
+
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
